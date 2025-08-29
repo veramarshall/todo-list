a5aa1ad1bf1da47f1e6d311ec50ee3d0bd371769
veramarshall <vera.marshall@ascte.org>
2025-08-29 11:15:40 -0500

    delete tasks working

--- todo.py
+++ todo.py
@@ -11,13 +11,14 @@ def add_task(task):
 def view_tasks():
     for i, task in enumerate(tasks, start=1):
         print(f"{i}. (task)")
-# Step 4: Delete a task
 
+# Step 4: Delete a task
+def delete_tasks(index):
+    task = tasks.pop(index)
+    print(f"Deleted: {task}")
 
 # Step 5: Mark task complete
-
-
-# Step 6: Save/load tasks (extra stretch for today)
+def complete_tasks():
 
 
 # Demo flow (you can run this file directly: python todo.py)
@@ -25,6 +26,8 @@ if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
-    mark_complete(0)
+    delete_tasks(0)
     view_tasks()
-    save_tasks()
+    # mark_complete(0)
+    # view_tasks()
+    # save_tasks()
